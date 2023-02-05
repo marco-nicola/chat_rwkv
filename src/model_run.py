5ee2c13834c55565bba4f17a1fa8f5363c9402f0
Marco Nicola <marconicola@disroot.org>
2023-02-05 18:40:56 +0100

    Refactor model_run.py

--- src/model_run.py
+++ src/model_run.py
@@ -1,27 +1,28 @@
-########################################################################################################
-# The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
-########################################################################################################
-
+import gc
+import os
 import types
+
 import torch
-import math, os, gc
 from torch.nn import functional as F
 import torch.nn as nn
 
 MyModule = nn.Module
+
+
 def __nop(ob):
     return ob
+
+
 MyFunction = __nop
 
-if os.environ["RWKV_JIT_ON"] == "1":
+if os.environ['RWKV_JIT_ON'] == '1':
     MyModule = torch.jit.ScriptModule
     MyFunction = torch.jit.script_method
 
 print(f'\nRWKV_JIT_ON {os.environ["RWKV_JIT_ON"]}\n')
 
-RWKV_RESCALE_LAYER = 6 # set x = x/2 every X layer (to avoid FP16 overflow)
+RWKV_RESCALE_LAYER = 6  # set x = x/2 every X layer (to avoid FP16 overflow)
 
-############################################################################################################
 
 class RWKV_RNN(MyModule):
     def __init__(self, args):
@@ -49,7 +50,7 @@ class RWKV_RNN(MyModule):
                     w[x] = w[x] / (2 ** int(block_id // RWKV_RESCALE_LAYER))
                 if 'ffn.value.weight' in x:
                     w[x] = w[x] / (2 ** int(block_id // RWKV_RESCALE_LAYER))
-                                
+
                 if '.time_' in x:
                     w[x] = w[x].squeeze()
                 if '.time_decay' in x:
@@ -68,12 +69,12 @@ class RWKV_RNN(MyModule):
 
                 if ('blocks.' not in x) or ('blocks.0.' in x):
                     if print_need_newline:
-                        print('\n', end = '')
+                        print('\n', end='')
                         print_need_newline = False
                     print(x.ljust(40), str(w[x].dtype).replace('torch.', '').ljust(10), w[x].device)
                 else:
                     print_need_newline = True
-                    print('.', end = '', flush = True)
+                    print('.', end='', flush=True)
 
         # store weights in self.w
         keys = list(w.keys())
@@ -91,7 +92,7 @@ class RWKV_RNN(MyModule):
                     if i == len(xx) - 1:
                         setattr(here, xx[i], w[x])
                     elif not hasattr(here, xx[i]):
-                        if xx[i+1].isdigit():
+                        if xx[i + 1].isdigit():
                             setattr(here, xx[i], {})
                         else:
                             setattr(here, xx[i], types.SimpleNamespace())
@@ -107,11 +108,11 @@ class RWKV_RNN(MyModule):
     # state[] 0=ffn_xx 1=att_xx 2=att_aa 3=att_bb 4=att_pp
 
     @MyFunction
-    def FF_one(self, x, state, i:int, time_mix_k, time_mix_r, kw, vw, rw):
-        xx = state[5*i+0].to(dtype=self.FLOAT_MODE)
+    def FF_one(self, x, state, i: int, time_mix_k, time_mix_r, kw, vw, rw):
+        xx = state[5 * i + 0].to(dtype=self.FLOAT_MODE)
         xk = x * time_mix_k + xx * (1 - time_mix_k)
         xr = x * time_mix_r + xx * (1 - time_mix_r)
-        state[5*i+0] = x.float()
+        state[5 * i + 0] = x.float()
 
         r = torch.sigmoid(xr @ rw)
         k = torch.square(torch.relu(xk @ kw))
@@ -119,11 +120,11 @@ class RWKV_RNN(MyModule):
         return r * kv
 
     @MyFunction
-    def FF_seq(self, x, state, i:int, time_mix_k, time_mix_r, kw, vw, rw):
-        xx = torch.cat((state[5*i+0].to(dtype=self.FLOAT_MODE).unsqueeze(0), x[:-1,:]))
+    def FF_seq(self, x, state, i: int, time_mix_k, time_mix_r, kw, vw, rw):
+        xx = torch.cat((state[5 * i + 0].to(dtype=self.FLOAT_MODE).unsqueeze(0), x[:-1, :]))
         xk = x * time_mix_k + xx * (1 - time_mix_k)
         xr = x * time_mix_r + xx * (1 - time_mix_r)
-        state[5*i+0] = x[-1,:].float()
+        state[5 * i + 0] = x[-1, :].float()
 
         r = torch.sigmoid(xr @ rw)
         k = torch.square(torch.relu(xk @ kw))
@@ -131,20 +132,20 @@ class RWKV_RNN(MyModule):
         return r * kv
 
     @MyFunction
-    def SA_one(self, x, state, i:int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
-        xx = state[5*i+1].to(dtype=self.FLOAT_MODE)
+    def SA_one(self, x, state, i: int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
+        xx = state[5 * i + 1].to(dtype=self.FLOAT_MODE)
         xk = x * time_mix_k + xx * (1 - time_mix_k)
         xv = x * time_mix_v + xx * (1 - time_mix_v)
         xr = x * time_mix_r + xx * (1 - time_mix_r)
-        state[5*i+1] = x.float()
+        state[5 * i + 1] = x.float()
 
         r = torch.sigmoid(xr @ rw)
         k = (xk @ kw).float()
         v = (xv @ vw).float()
 
-        aa = state[5*i+2]
-        bb = state[5*i+3]
-        pp = state[5*i+4]
+        aa = state[5 * i + 2]
+        bb = state[5 * i + 3]
+        pp = state[5 * i + 4]
         ww = time_first + k
         p = torch.maximum(pp, ww)
         e1 = torch.exp(pp - p)
@@ -155,27 +156,27 @@ class RWKV_RNN(MyModule):
         p = torch.maximum(ww, k)
         e1 = torch.exp(ww - p)
         e2 = torch.exp(k - p)
-        state[5*i+2] = e1 * aa + e2 * v
-        state[5*i+3] = e1 * bb + e2
-        state[5*i+4] = p
+        state[5 * i + 2] = e1 * aa + e2 * v
+        state[5 * i + 3] = e1 * bb + e2
+        state[5 * i + 4] = p
         wkv = (a / b).to(dtype=self.FLOAT_MODE)
         return (r * wkv) @ ow
 
     @MyFunction
-    def SA_seq(self, x, state, i:int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
-        xx = torch.cat((state[5*i+0].to(dtype=self.FLOAT_MODE).unsqueeze(0), x[:-1,:]))
+    def SA_seq(self, x, state, i: int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
+        xx = torch.cat((state[5 * i + 0].to(dtype=self.FLOAT_MODE).unsqueeze(0), x[:-1, :]))
         xk = x * time_mix_k + xx * (1 - time_mix_k)
         xv = x * time_mix_v + xx * (1 - time_mix_v)
         xr = x * time_mix_r + xx * (1 - time_mix_r)
-        state[5*i+1] = x[-1,:].float()
+        state[5 * i + 1] = x[-1, :].float()
 
         r = torch.sigmoid(xr @ rw)
         k = (xk @ kw).float()
         v = (xv @ vw).float()
 
-        aa = state[5*i+2]
-        bb = state[5*i+3]
-        pp = state[5*i+4]
+        aa = state[5 * i + 2]
+        bb = state[5 * i + 3]
+        pp = state[5 * i + 4]
         T = x.shape[0]
         for t in range(T):
             ww = time_first + k[t]
@@ -193,13 +194,13 @@ class RWKV_RNN(MyModule):
                 bb = e1 * bb + e2
                 pp = p
             else:
-                state[5*i+2] = e1 * aa + e2 * v[t]
-                state[5*i+3] = e1 * bb + e2
-                state[5*i+4] = p
+                state[5 * i + 2] = e1 * aa + e2 * v[t]
+                state[5 * i + 3] = e1 * bb + e2
+                state[5 * i + 4] = p
             xx[t] = (a / b).to(dtype=self.FLOAT_MODE)
         return (r * xx) @ ow
 
-    def forward(self, tokens, state, preprocess_only = False):
+    def forward(self, tokens, state, preprocess_only=False):
         with torch.no_grad():
             w = self.w
             args = self.args
@@ -213,7 +214,7 @@ class RWKV_RNN(MyModule):
             if state == None:
                 state = torch.zeros(args.n_layer * 5, args.n_embd, device=self.RUN_DEVICE)
                 for i in range(args.n_layer):
-                    state[5*i+4] -= 1e30
+                    state[5 * i + 4] -= 1e30
 
             SA = self.SA_seq if seq_mode else self.SA_one
             FF = self.FF_seq if seq_mode else self.FF_one
@@ -221,24 +222,24 @@ class RWKV_RNN(MyModule):
             for i in range(args.n_layer):
                 if i == 0:
                     x = self.LN(x, w.blocks[i].ln0)
-                
+
                 ww = w.blocks[i].att
-                x = x + SA(self.LN(x, w.blocks[i].ln1), state, i, 
-                    ww.time_mix_k, ww.time_mix_v, ww.time_mix_r, ww.time_first, ww.time_decay, 
-                    ww.key.weight, ww.value.weight, ww.receptance.weight, ww.output.weight)
-                
+                x = x + SA(self.LN(x, w.blocks[i].ln1), state, i,
+                           ww.time_mix_k, ww.time_mix_v, ww.time_mix_r, ww.time_first, ww.time_decay,
+                           ww.key.weight, ww.value.weight, ww.receptance.weight, ww.output.weight)
+
                 ww = w.blocks[i].ffn
-                x = x + FF(self.LN(x, w.blocks[i].ln2), state, i, 
-                    ww.time_mix_k, ww.time_mix_r, 
-                    ww.key.weight, ww.value.weight, ww.receptance.weight)
-                
-                if (i+1) % RWKV_RESCALE_LAYER == 0:
+                x = x + FF(self.LN(x, w.blocks[i].ln2), state, i,
+                           ww.time_mix_k, ww.time_mix_r,
+                           ww.key.weight, ww.value.weight, ww.receptance.weight)
+
+                if (i + 1) % RWKV_RESCALE_LAYER == 0:
                     x = x / 2
 
             if preprocess_only:
                 return state
 
-            x = self.LN(x[-1,:], w.ln_out) if seq_mode else self.LN(x, w.ln_out)
+            x = self.LN(x[-1, :], w.ln_out) if seq_mode else self.LN(x, w.ln_out)
             x = w.head.weight @ x
 
             return x.float(), state
